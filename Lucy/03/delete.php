43aeba899a3d11e0330f80a8d036f6a8680272d0
killerlucy <31403702+killerlucy@users.noreply.github.com>
2017-08-29 21:15:59 +0800

    郎珊作业
    3&4

---
+++ Lucy/03/delete.php
@@ -0,0 +1,24 @@
+<?php 
+require "MySql.class.php";
+$db = new MySql;
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM student WHERE uid='$id'";
+// echo $sql;
+$res = $db->delete($sql);
+// var_dump($res);
+if($res === false)
+{
+	// echo $db->error;
+	echo '删除Fail';
+}
+else
+{
+	echo '删除OK';
+
+}
+
+
+?>
+
---
+++ Lucy/04/upload.php
@@ -0,0 +1,43 @@
+<?php 
+header("Content-type:text/html;charset=utf-8;");
+
+require "MySql.class.php";
+require "Upload.class.php";
+
+$db = new MySql();
+$up = new Upload();
+$result = $up->uploadOne($_FILES);
+
+$imgname = $_POST['imgname'];
+//var_dump($result);     
+
+if($result)
+{
+	/**
+	 * 需要
+	 *
+	 * 成功返回：上传后的路径
+	 * 失败返回：1.FALSE
+	 * 			 2.可以获取到错误消息
+	 */
+
+	// 入库
+	$sql = "INSERT INTO photo (name,img) VALUES ('$imgname','$result')";
+	$res = $db->insert($sql);
+	var_dump($res);
+}
+else
+{
+	// 输出：错误消息
+	echo $up->getError();
+}
+
+
+
+
+
+
+
+
+
+?>
