173227b5f6a9c80097b0933434c2494430eda252
flyname <30924355+flyname@users.noreply.github.com>
2017-08-29 21:21:30 +0800

    张志国
    第四天

---
+++ zhiguo/day4/2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="3.php" method="post" enctype="multipart/form-data">
+		<table>
+			<tr>
+				<td>图片名称</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>图片</td>
+				<td><input type="file" name='img'></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="提交"></td>
+				<td></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
