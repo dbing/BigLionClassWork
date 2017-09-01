798160cafbbf4ec75a108aadbbf80833f50ce3a7
xiaokongxiang <2254688890@qq.com>
2017-09-01 08:23:07 +0800

    lixin
    0830

---
+++ lixin/0830/final.php
@@ -0,0 +1,22 @@
+﻿<?php 
+//李鑫
+//0830
+class Animal
+{
+	private $name;
+	final public function shout()
+	{
+		echo '嘻嘻嘻';
+	}
+}
+
+
+class Cat extends Animal
+{
+
+}
+
+$cat = new Cat;
+$cat->shout();
+
+?>
