ba7eb19e492f9d5b2a891685acbf9ae1d382eb36
flyname <30924355+flyname@users.noreply.github.com>
2017-09-07 15:28:19 +0800

    11
    11

--- zhiguo/day10/1.php
+++
@@ -1,9 +0,0 @@
-<!-- 张志国 -->
-<?php
-
-
-$title='标题';
-$content='兴趣';
-require '2.html';
-?>
-
--- zhiguo/day10/add_form.php
+++
@@ -1,8 +0,0 @@
-<!-- 张志国 -->
-<?php
-
-require 'smarty/Smarty.class.php';
-
-$smarty = new Smarty;
-$smarty->template_dir = 'tpl';
-$smarty->display('admin_add.html');
